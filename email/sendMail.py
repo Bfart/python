1edd25f777bc623203b6617452f9326907c52fef
Bfart <ziliu1991@163.com>
2016-07-15 16:39:31 +0800

    Update sendMail.py

--- email/sendMail.py
+++ email/sendMail.py
@@ -3,17 +3,17 @@ import smtplib
 from email.mime.text import MIMEText  
 from email.header import Header  
   
-sender = 'ziliu1991@163.com'  
-receiver = 'Bfart1991@163.com'  
+sender = '***@163.com'  
+receiver = '***@163.com'  
 subject = '放假通知'  
 smtpserver = 'smtp.163.com'  
-username = 'ziliu1991@163.com'  
-password = 'liuhui520'  
+username = '***@163.com'  
+password = '****'  
   
 msg = MIMEText('大家好，今晚聚餐','plain','utf-8')#中文需参数‘utf-8'，单字节字符不需要  
 msg['Subject'] = Header(subject, 'utf-8')  
-msg['From'] = 'ziliu1991@163.com'   
-msg['To'] = 'Bfart1991@163.com'   
+msg['From'] = '***@163.com'   
+msg['To'] = '*****@163.com'   
 smtp = smtplib.SMTP()  
 smtp.connect('smtp.163.com')  
 smtp.login(username, password)  
